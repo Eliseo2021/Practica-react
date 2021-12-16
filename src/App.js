db6e3616d05f2a652e06cfd2f9971f553f7730f5
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2021-12-16 18:07:05 -0300

    2Componentes funcionales

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+function Saludo() {
+  return <h1>Hola Mundo!</h1>;
+};
+
 
 
 function App() {
   return (
-    <h1>Hola React</h1>
+    <>
+      <h1>Hola React</h1>
+      <Saludo />
+    </>
   );
 };
 
